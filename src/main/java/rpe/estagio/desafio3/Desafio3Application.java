06d783c3b7e4c21582449a39faaffa39fabdba2d
Logandi <59068101+BrandonLogandi@users.noreply.github.com>
2023-01-09 18:52:44 -0300

    🎉first commit

---
+++ src/main/java/rpe/estagio/desafio3/Desafio3Application.java
@@ -0,0 +1,13 @@
+package rpe.estagio.desafio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/rpe/estagio/desafio3/Desafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package rpe.estagio.desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
