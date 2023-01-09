46bde69a58e76b409482fa013e65db2fdec90167
Logandi <59068101+BrandonLogandi@users.noreply.github.com>
2023-01-09 19:00:15 -0300

    🔧chore: configure Swagger & DB

--- src/main/java/rpe/estagio/desafio3/Desafio3Application.java
+++ src/main/java/rpe/estagio/desafio3/Desafio3Application.java
@@ -3,7 +3,11 @@ package rpe.estagio.desafio3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Estágio Java RPE - Desafio 3", version = "1.0", description = "Solução do Desafio 3 do Estágio Java - RPE"))
 public class Desafio3Application {
 
 	public static void main(String[] args) {
